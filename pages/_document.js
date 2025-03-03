767d375dfc52e00a3543b17c4a8401657429e394
zaunist <y.bz@foxmail.com>
2025-03-03 22:41:31 +0800

    feat: add clarity support

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,19 @@ class MyDocument extends Document {
     return (
       <Html lang={BLOG.LANG}>
         <Head>
+          {/* Clarity 跟踪代码 */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "qihe6csyb9");
+              `
+            }}
+          />
+
           {/* 预加载字体 */}
           {BLOG.FONT_AWESOME && (
             <>
