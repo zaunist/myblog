296b6c4802ef35c701ecc02cc97c8632bdbbaa8f
zaunist <y.bz@foxmail.com>
2025-06-06 02:57:47 +0800

    fix: social link

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -2,8 +2,8 @@
  * 社交按钮相关的配置同意放这
  */
 module.exports = {
-  CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://github.com/zaunist', // 你的twitter个人主页
+  CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://x.com/zaunist', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/zaunist', // 你的github个人主页 例如 https://github.com/tangly1024
-  CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://github.com/zaunist', // Youtube主页
+  CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://www.youtube.com/@zaunist', // Youtube主页
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/zaunist'
 }
